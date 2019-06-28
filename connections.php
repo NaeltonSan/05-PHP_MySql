aa30a19589c76247eb16654fa6fc233c386e3a48
Naelton Santos <naeon02@gmail.com>
2019-06-27 22:50:47 -0300

    conectando com o banco de dados

---
+++ connections.php
@@ -0,0 +1,34 @@
+<?php
+
+// ini_set('mysqli.allow_persistent','On');     //usado quando o tempo de conexões no banco de dados estoura!
+// ini_set('mysql.max_persistent', -1);
+// ini_set('mysql.max_links', -1);
+
+
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$database = "php_mysql_iniciando";
+
+$conexao = new mysqli($servidor, $usuario, $senha, $database);
+
+// if ($conexao) {
+//     echo $conexao->host_info;
+// }
+
+echo "<br>";
+
+$sql = 'CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL
+)';
+
+// if (!$conexao->query($sql)){
+//     echo "Tabela Users já existe.";
+// }
+
+echo "<br>";
+
+$result = $conexao->query('INSERT INTO users (email) VALUE ("naelton@teste.com")');
+
+var_dump($result);
