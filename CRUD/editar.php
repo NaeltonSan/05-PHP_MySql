240cb438423aee1928007dcce620fabed07ac0ff
Naelton Santos <naeon02@gmail.com>
2019-06-29 10:44:31 -0300

    criação do editar

---
+++ CRUD/editar.php
@@ -0,0 +1,36 @@
+<?php
+$conexao = require('connection.php');
+$id = $_GET['id'] ?? null;
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'] ?? null;
+    $stmt = $conexao->prepare('UPDATE users SET email=? WHERE id=?');
+    $stmt->bind_param('si', $email, $id);
+    $stmt->execute();
+    header('location: /PHP_SON/05-PHP_MySQL/CRUD');
+    die();
+}
+$stmt = $conexao->prepare('SELECT * FROM users WHERE id=?');
+$stmt->bind_param('i', $id);
+$stmt->execute();
+$result = $stmt->get_result();
+$user = $result->fetch_assoc();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Editar usuário</h1>
+
+    <form action="/PHP_SON/05-PHP_MySQL/CRUD/editar.php?id=<?php echo $user['id']; ?>" method="post">
+        <input type="text" name="email" value="<?php echo $user['email']; ?>">
+        <input type="submit" value="enviar">
+    </form>
+
+    <p><a href="/PHP_SON/05-PHP_MySQL/CRUD">Voltar</a></p>
+</body>
+</html>
