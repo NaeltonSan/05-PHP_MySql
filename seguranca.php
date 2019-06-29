fcc45f719dc195478329bb7e39f31f8f632dcea0
Naelton Santos <naeon02@gmail.com>
2019-06-28 23:13:25 -0300

    prepared statement

--- seguranca.php
+++ seguranca.php
@@ -10,10 +10,21 @@
 
     // $result = $conexao->query('INSERT INTO users (email) VALUES ("'. $email.'")');
 
-    $result = $conexao->query('SELECT * FROM users WHERE id > 5');
-    $users = $result->fetch_all(MYSQLI_ASSOC); // traz o nome dos campos, neste caso ID e email, ao inves de nuemros  0,1,2...
+    $stmt = $conexao->prepare('SELECT * FROM users where id > ? and id < ?'); // coloca ?
+    /**
+     * i = integer
+     * s = string
+     * b = blob
+     * d = double
+     */
+    $stmt->bind_param('ii', $id, $id2);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $users = $result->fetch_all(MYSQLI_ASSOC);
     foreach ($users as $user) {
         echo $user['id'] . ' - ' . $user['email'] . '</br>';
     }
 
+    echo "<pre>";
+    var_dump($users);
 ?>
